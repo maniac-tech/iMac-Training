dddfc7cc1f91059b63b38c2dc80fa73e1a912b8f
admin <admin@ACE03.local>
2017-03-30 12:59:56 +0530

    Created basic todolist

---
+++ To-Do-List/To-Do-List/FirstViewController.swift
@@ -0,0 +1,39 @@
+//
+//  FirstViewController.swift
+//  To-Do-List
+//
+//  Created by admin on 30/03/17.
+//  Copyright © 2017 ACE. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    //TableView functions:
+    @available(iOS 2.0, *)
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return 3;
+    }
+    
+    @available(iOS 2.0, *)
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = UITableViewCell(style:UITableViewCellStyle.default, reuseIdentifier: "cell");
+        cell.textLabel?.text=String(indexPath.row)
+        return cell;
+    }
+    
+    @IBOutlet weak var toDoList: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ To-Do-List/To-Do-List/SecondViewController.swift
@@ -0,0 +1,28 @@
+//
+//  SecondViewController.swift
+//  To-Do-List
+//
+//  Created by admin on 30/03/17.
+//  Copyright © 2017 ACE. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var inputToDo: UITextField!
+    @IBAction func saveToDo(_ sender: AnyObject) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
