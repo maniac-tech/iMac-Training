52ec20cadc77499f959eb3169291757bc9e84c7e
maniac-tech <ajj_2396@yahoo.com>
2017-03-31 12:30:33 +0530

    Final update, added text and map address

--- lost-and-found/lost-and-found/ViewController.swift
+++ lost-and-found/lost-and-found/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     
     @IBOutlet weak var lattitude: UILabel!
     @IBOutlet weak var longitude: UILabel!
-    @IBOutlet weak var attitude: UILabel!
+    @IBOutlet weak var altitiude: UILabel!
     @IBOutlet weak var speed: UILabel!
     @IBOutlet weak var course: UILabel!
     @IBOutlet weak var address: UILabel!
@@ -27,11 +27,68 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        locationManagingVar.delegate=self
+        locationManagingVar.desiredAccuracy=kCLLocationAccuracyBest
+        locationManagingVar.requestWhenInUseAuthorization() // to request usage when required
+        locationManagingVar.startUpdatingLocation()
+        
     }
 
     //locationManager function:
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let userlocation = locations[0]
+        let userlatitude = userlocation.coordinate.latitude
+        let userlongitude = userlocation.coordinate.longitude
+        let latdelta : CLLocationDegrees = 0.05
+        let longdelta :CLLocationDegrees = 0.05
+        let span = MKCoordinateSpan(latitudeDelta: latdelta, longitudeDelta: longdelta)
+        let location = CLLocationCoordinate2D(latitude: userlatitude, longitude: userlongitude)
+        let region = MKCoordinateRegion(center: location, span: span)
         
+        map.setRegion(region, animated: true)
+        //print(locations) //to print the current location readings into the console
+        //print(userlocation) //tp print extracted data from the array of locations
+        //print(userlatitude)
+        //print(userlongitude)
+        
+        //setting the labels with the required information in the labels:
+        lattitude.text=String (userlatitude)
+        longitude.text=String(userlongitude)
+        altitiude.text=String(userlocation.altitude)
+        speed.text=String(userlocation.speed)
+        course.text=String(userlocation.course)
+        
+        CLGeocoder().reverseGeocodeLocation(userlocation){
+            (placemarks, errors)in
+            if errors != nil {
+                print (errors)
+            }
+            else{
+                //print(placemarks)
+                if let placemark = placemarks?[0]{
+                    var address=" "
+                    if placemark.subThoroughfare != nil {
+                        address += placemark.subThoroughfare! + " "
+                    }
+                    if placemark.thoroughfare != nil {
+                        address += placemark.thoroughfare! + " "
+                    }
+                    if placemark.subLocality != nil {
+                        address += placemark.subLocality! + " "
+                    }
+                    if placemark.locality != nil {
+                        address += placemark.locality! + " "
+                    }
+                    if placemark.postalCode != nil {
+                        address += placemark.postalCode! + " "
+                    }
+                    if placemark.country != nil {
+                        address += placemark.country! + " "
+                    }
+                    self.address.text=address
+                }
+            }
+        }
     }
     
     override func didReceiveMemoryWarning() {
