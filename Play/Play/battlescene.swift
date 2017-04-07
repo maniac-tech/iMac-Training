08705453258b75f44810f4ea17580b0cedf0b9fa
maniac-tech <ajj_2396@yahoo.com>
2017-04-07 15:08:34 +0530

    Added pokemon and pokeball to the battle scene

--- Play/Play/battlescene.swift
+++ Play/Play/battlescene.swift
@@ -13,6 +13,16 @@ import SpriteKit
 
 public class battlescene : SKScene, SKPhysicsContactDelegate{
     var pokemon : Pokemon!
+    var pokemonSprite : SKSpriteNode!
+    var pokeballSprite : SKSpriteNode!
+    
+    //defining constants
+    let kPokemonSize = CGSize(width : 100, height : 100)
+    let kPokeballSize = CGSize(width : 100, height : 100)
+    
+    //define bitCategories
+    
+    //physicas variable setup
     
     //we have didLoad in any ViewController, but here we have a game scene 
     //and hence, we use the following function which checks if there was any movement in the current gamescene:
@@ -30,5 +40,42 @@ public class battlescene : SKScene, SKPhysicsContactDelegate{
         
         //adding the battle background to the scene:
         self.addChild(battleBg)
+        
+        self.perform(#selector(setupPokemon), with: nil, afterDelay: 2.0)
+        self.perform(#selector(setuppPokeball), with: nil, afterDelay: 2.0)
     }
+    
+    
+    //setting up pokemon and pokeball separately to avoid complexity in reading and understanding the code snippet while debugging
+    func setupPokemon(){
+        self.pokemonSprite = SKSpriteNode(imageNamed: pokemon.imageFileName!)
+        self.pokemonSprite.size = kPokemonSize
+        self.pokemonSprite.position = CGPoint(x: self.size.width/2, y: self.size.height/2)
+        
+        self.pokemonSprite.zPosition = 0
+        
+        //adding physics i.e.movement to the pokemon:
+        let moveRight = SKAction.moveBy(x:100 , y: 0, duration: 3.0)
+        let sequence = SKAction.sequence([moveRight,moveRight.reversed(),moveRight.reversed(),moveRight])
+        
+        //Adding sequence action to the pokemon
+        self.pokemonSprite.run(SKAction.repeatForever(sequence))
+        
+        
+        //Adding the pokemon to BattleScene
+        self.addChild(pokemonSprite)
+        
+    }
+    
+    func setuppPokeball(){
+        self.pokeballSprite = SKSpriteNode(imageNamed: "pokeball")
+        self.pokeballSprite.size = kPokeballSize
+        self.pokeballSprite.position = CGPoint(x: self.size.width/2, y: 55)
+        
+        self.pokeballSprite.zPosition = 0
+        
+        //Adding the pokeball to BattleScene
+        self.addChild(pokeballSprite)
+    }
+    
 }
